8e0c388396d9a5f3094d0cb0eec88f8762a09461
cathy.simms <cathy.simms@weareams.com>
2023-01-04 11:57:45 +0000

    screenshots and file

---
+++ AWS/Terraform/task1-main.tf
@@ -0,0 +1,32 @@
+terraform {
+    required_providers {
+        aws = {
+            source =  "hashicorp/aws"
+            cersion = "~> 4.0"
+
+        }
+    }
+}
+
+locals {
+    ami_name = "ami-05e786af422f8082a"
+}
+
+variable "some_var" {
+    default = "hello"
+}
+
+provider "aws" {
+    shared_credentials_files = ["~/.aws/credentials"]
+    region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "${local.ami_name}"
+    instance_type = "t2.micro"
+    key_name = "AMSkey"
+}
+
+output "instance_arn" {
+    value = aws_instance.example.arn
+}
